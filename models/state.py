789513bba43755f1eea1b92cadb7305fb694a486
Majidied <majidimajidi2003@gmail.com>
2024-02-08 02:04:18 +0100

    all

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module creates a State Class"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """Class for managing state objects"""
+    
+    name = ""
---
+++ one/models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module creates a Review Class"""
+
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """Class for managing review objects"""
+    
+    place_id = ""
+    user_id = ""
+    text = ""
